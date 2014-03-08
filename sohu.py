8fe324992dbe2766548086a9ce4eedba4103febb
caorong <lelouchcr@gmail.com>
2014-03-08 12:48:00 +0800

    fix sohu

--- sohu.py
+++ sohu.py
@@ -4,10 +4,10 @@ __all__ = ['sohu_download']
 
 from common import *
 
-def real_url(host, prot, file, new):
+def real_url(host, prot, file, new, ch, catcode):
 	url = 'http://%s/?prot=%s&file=%s&new=%s' % (host, prot, file, new)
-	start, _, host, key, _, _ = get_html(url).split('|')
-	return '%s%s?key=%s' % (start[:-1], new, key)
+	start, _, host, key, _, _, n, a = get_html(url).split('|')
+	return '%s%s?key=%s&ch=%s&catcode=%s&n=%s&a=%s' % (start[:-1], new, key, ch, catcode, n, a)
 
 def sohu_download(url, merge=True):
 	vid = r1('vid="(\d+)"', get_html(url))
@@ -16,13 +16,15 @@ def sohu_download(url, merge=True):
 	data = json.loads(get_decoded_html('http://hot.vrs.sohu.com/vrs_flash.action?vid=%s' % vid))
 	host = data['allot']
 	prot = data['prot']
+	catcode = data['catcode']
 	urls = []
 	data = data['data']
 	title = data['tvName']
+	ch = data['ch']
 	size = sum(data['clipsBytes'])
 	assert len(data['clipsURL']) == len(data['clipsBytes']) == len(data['su'])
 	for file, new in zip(data['clipsURL'], data['su']):
-		urls.append(real_url(host, prot, file, new))
+		urls.append(real_url(host, prot, file, new, ch, catcode))
 	assert data['clipsURL'][0].endswith('.mp4')
 	download_urls(urls, title, 'mp4', total_size=size, refer=url, merge=merge)
 
